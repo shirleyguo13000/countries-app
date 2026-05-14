d142998b180b080444301cf9b03c87b2741ec698
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-05-14 17:14:20 -0400

    completed api rendering

--- version-1/src/App.jsx
+++ version-1/src/App.jsx
@@ -1,58 +1,66 @@
 import "./App.css";
-import { Routes, Route, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Home from './pages/Home';
-import CountryDetail from './pages/CountryDetail';
-import SavedCountries from './pages/SavedCountries';
-import localData from './localData';
-
+import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import CountryDetail from "./pages/CountryDetail";
+import SavedCountries from "./pages/SavedCountries";
+import localData from "./localData";
 
 function App() {
+  // state variable to store API data
   const [countries, setCountries] = useState([]);
 
-
-   const countryApi = async () => {
+  // API call. Using async so that each command waits for the action to be completed before moving on to next
+  const countryApi = async () => {
     try {
       const response = await fetch(
-        `https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region,cca3,borders`
+        `https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region,cca3,borders`,
       );
       const data = await response.json();
-      console.log('data', data);
-      setCountries(countryApi);
+      console.log("data", data);
+      setCountries(data);
+      // if API fails to load, countries data will be sourced from local js file
     } catch (error) {
-      console.log('error:', error.message);
+      console.log("error:", error.message);
+      setCountries(localData);
     }
-   };
-  
-       useEffect(() => {
-       countryApi();
-     }, []);
-  
-  
+  };
+
+  // API runs once upon loading page with useEffect
+  useEffect(() => {
+    countryApi();
+  }, []);
+
   return (
     <>
       <div>
         <header>
-      <nav>
-        <ul>
-            <li>
-            <Link  to="/" className="link1">Where in the World?</Link>
-          </li>
-          <li>
-            <Link  to="/SavedCountries" className="link2">Saved Countries</Link>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/" className="link1">
+                  Where in the World?
+                </Link>
+              </li>
+              <li>
+                <Link to="/SavedCountries" className="link2">
+                  Saved Countries
+                </Link>
               </li>
             </ul>
-            </nav>
+          </nav>
         </header>
-        
-     
-   
-      <Routes>
-        <Route path="/" element={ <Home countriesData={countries} />} />
-          <Route path="/CountryDetail/:countryName" element={<CountryDetail countriesData={countries}/>} />
+
+        <Routes>
+          {/* linked up props to api data thats stored in "countries" state variable */}
+          <Route path="/" element={<Home countriesData={countries} />} />
+          <Route
+            path="/CountryDetail/:countryName"
+            element={<CountryDetail countriesData={countries} />}
+          />
           <Route path="/SavedCountries" element={<SavedCountries />} />
-      </Routes>
-    </div>
+        </Routes>
+      </div>
     </>
   );
 }
--- version-1/src/components/CountryCard.jsx
+++ version-1/src/components/CountryCard.jsx
@@ -1,19 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "../App.css";
 
 function CountryCard({ country }) {
-    console.log("data from country",country);
+  // console.log("data from country",country);
     return (
-        <Link to={`/CountryDetail/${country.name.common}`}>
-            <img src={country.flags.png} alt="flag" className="country-card-img" />
-            <div className="country-card-lowerhalf">
-            <h3 className="country-card-h3">{country.name.common}</h3>
-            <p className="country-card-p">Population: {country.population}</p>
-            <p className="country-card-p">Capital: {country.capital}</p>
-            <p className="country-card-p">Region: {country.region}</p>
-            </div>
-    </Link >
-    )
+    //  Dynamic router to make URL update according to country name in UI link
+    <Link to={`/CountryDetail/${country.name.common}`}>
+      <div className="country-card">
+        <img src={country.flags.png} alt="flag" className="country-card-img" />
+        <div className="country-card-lowerhalf">
+          <h3 className="country-card-h3">{country.name.common}</h3>
+          <p className="country-card-p">Population: {country.population}</p>
+          <p className="country-card-p">Capital: {country.capital}</p>
+          <p className="country-card-p">Region: {country.region}</p>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default CountryCard;
