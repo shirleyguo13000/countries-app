61cfe64cb100ab5a001c49a9ade880d1e998acf3
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-08-02 21:06:53 -0400

    deployed and updated everything

--- version-4/client/vite.config.js
+++ version-4/client/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
+    proxy: {https://html-css-js.com/css/generator/text-shadow/
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://countries-app-zh92.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
