ac2397911ae4650c09df2efbdafdd316b27b8db6
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-08-02 22:42:03 -0400

    final try

--- version-4/client/vite.config.js
+++ version-4/client/vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: {https://html-css-js.com/css/generator/text-shadow/
+    proxy: {
       "/api": {
         target: "https://countries-app-zh92.onrender.com",
         changeOrigin: true,
